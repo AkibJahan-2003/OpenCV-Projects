467b5f165e4087b8241050ecf54669ce76c1bb49
Md Akib Jahan <akibjahan12345@gmail.com>
2026-01-02 23:48:36 +0600

    Add files via upload

---
+++ Library book counter/BookSpacePicker.py
@@ -0,0 +1,42 @@
+import cv2
+import pickle
+
+width, height = 115, 630
+
+# This will make save
+try:
+    with open('libraryBookPos', 'rb') as f:
+        posList = pickle.load(f)
+except:
+    posList = []
+
+
+# mouse click function
+def mouseClick(events, x, y, flags, params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                posList.pop(i)
+
+    with open('libraryBookPos', 'wb') as f:
+        pickle.dump(posList, f)
+
+
+while True:
+    img = cv2.imread("book.jfif")
+    img = cv2.resize(img, (1280, 720))
+
+    # create rectangle
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+    cv2.imshow("Image", img)
+
+    # mouse click
+    cv2.setMouseCallback("Image", mouseClick)
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
+        break
+cv2.destroyAllWindows()
